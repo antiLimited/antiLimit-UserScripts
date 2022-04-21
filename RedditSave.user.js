383a54374d43fa4e51af8d01b51819a70655352e
antiLimitGit <87735561+antiLimitGit@users.noreply.github.com>
2022-04-21 12:12:32 -0400

    Create RedditSave.user.js
    A slightly more efficient way of saving videos on Reddit, press alt-s on whatever post you are on and you will be sent to redditsave.com to download the video.

---
+++ RedditSave.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Video Save - reddit.com
+// @namespace   Violentmonkey Scripts
+// @match       https://www.reddit.com/*
+// @grant       none
+// @version     1.0
+// @author      antiLimit
+// @description 4/21/2022, 11:49:43 AM
+// ==/UserScript==
+
+document.onkeyup = function(e) {
+  if (e.altKey && e.which == 83) {
+    alert("Forwarding you to redditsave.com");
+    window.location.href = `https://redditsave.com/info?url=${window.location.href}`
+  }
+};
